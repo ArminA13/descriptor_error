2696e53ca2a8f990d04af8157aaa87ade8f4557f
Armine Muradyan <muradyan.armin13@gmail.com>
2023-09-16 11:58:57 +0400

    add error3 function

---
+++ error3.js
@@ -0,0 +1,19 @@
+// Convert the code using try...catch.
+
+
+function reverseString(s) {
+    try {
+        if(typeof s !== "string") {
+            throw new Error("s.split is not a function");
+        }
+
+        s = s.split("").reverse().join("");
+        console.log(s);
+
+    } catch(error) {
+        console.log(error.message);
+    }
+}
+
+reverseString("abc");
+reverseString(123);
